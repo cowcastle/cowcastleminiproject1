ddad44628265d6f060b423bc6063e1e564907d65
qlswltjdhandong <22100349@handong.ac.kr>
2022-05-06 12:27:24 +0900

    create addPrivacy version1

---
+++ srcc.c
@@ -0,0 +1,18 @@
+#include "srcc.h"
+
+int addPrivacy(Privacy *s){
+    printf("이름은? ");
+    scanf("%s",s->name);
+    printf("나이는? ");
+    scanf("%d",&s->age);
+    printf("전화번호는? ");
+    scanf("%s",s->phone_num);
+    printf("직책은?");
+    scanf("%s",s->position);
+    printf("고유번호는?");
+    scanf("%d",&s->special_num);
+    printf("비밀번호는?");
+    scanf("%s",s->pwd);
+
+    return 1;
+}
