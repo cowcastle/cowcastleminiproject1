a6c075332036c92ddd51c6eb18a8f6e88ea89353
qlswltjdhandong <22100349@handong.ac.kr>
2022-05-08 07:52:21 +0900

    add function readPrivacy read_OnePrivacy change original readPrivacy to seeTime

--- srcc.h
+++ srcc.h
@@ -15,13 +15,15 @@ typedef struct {
 } Privacy;
  
 int addPrivacy(Privacy *s);//1번째 사람 정보 추가하는 함수
+void readPrivacy(Privacy *s,int count);//13번째 사람 정보 보는 함수
+void read_OnePrivacy(Privacy s);
 int updatePrivacy(Privacy *s);//3번째 사람 정보 수정하는 함수
 int deletePrivacy(Privacy *s);//2번째 사람 정보 삭제하는 함수
 void saveData(Privacy *s,int count);//4번째 파일 정보 저장 함수
 int loadData(Privacy *s);//5번째 파일에 있는 데이터를 불러오는 함수
 int selectMenu();//6번째 출근 관련 전체메뉴
 void checkCommute(Privacy *s);//7번째 출퇴근 인증하는 함수
-void readPrivacy(Privacy *s);//8번째 출퇴근 기록보는 함수
+void seeTime(Privacy *s);//8번째 출퇴근 기록보는 함수
 void expressWrong(Privacy *s);//9번째 출근하고 퇴근 잘못 찍었을 때 표현해주는 함수
 int checkLate(Privacy *s);//10번째 지각한 사람 체크 함수
 void currentTime();//11번째 현재 시간을 알려주고 기록하는 함수
