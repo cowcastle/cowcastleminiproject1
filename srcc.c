73fff44699f75e8c0bbdc995adbe40aa7c845dcc
qlswltjdhandong <22100349@handong.ac.kr>
2022-05-07 22:33:28 +0900

    create addPrivacy version2

--- srcc.c
+++ srcc.c
@@ -1,17 +1,17 @@
 #include "srcc.h"
 
 int addPrivacy(Privacy *s){
-    printf("이름은? ");
+    printf("이름을 입력해주세요!! ");
     scanf("%s",s->name);
-    printf("나이는? ");
+    printf("나이를 입력해주세요!! ");
     scanf("%d",&s->age);
-    printf("전화번호는? ");
+    printf("전화번호를 입력해주세요!! ");
     scanf("%s",s->phone_num);
-    printf("직책은?");
+    printf("직책을 입력해주세요!! ");
     scanf("%s",s->position);
-    printf("고유번호는?");
+    printf("고유번호를 입력해주세요!! ");
     scanf("%d",&s->special_num);
-    printf("비밀번호는?");
+    printf("비밀번호를 입력해주세요");
     scanf("%s",s->pwd);
 
     return 1;
@@ -19,4 +19,18 @@ int addPrivacy(Privacy *s){
 int delete_Privacy(Privacy *s){
     s->age = -1;//나이가 음수인 거는 말이 되지 않음
   return 1;
+}
+int updatePrivacy(Privacy *s)
+{
+    printf("새로운 이름을 입력하세요 ");
+    scanf("%s", s->name);
+    printf("새로운 나이를 입력하세요 ");
+    scanf("%d", &s->age);
+    printf("직책을 입력하세요 ");
+    scanf("%d", &s->position);
+    printf("고유번호를 입력하세요 ");
+    scanf("%d", &s->special_num);
+    printf("==> 수정되었습니다 <== \n");
+
+    return 1;
 }
