dbc82d85eccc1ed393005c0837e68e7354fbd64a
Losecow <100589396+Losecow@users.noreply.github.com>
2022-05-07 23:07:38 +0900

    Merge branch 'main' of https://github.com/Losecow/cowcastleminiproject1

--- srcc.c
+++ srcc.c
@@ -1,37 +1,39 @@
 #include "srcc.h"
 
-int addPrivacy(Privacy *s){
-    printf("이름을 입력해주세요!! ");
-    scanf("%s",s->name);
-    printf("나이를 입력해주세요!! ");
-    scanf("%d",&s->age);
-    printf("전화번호를 입력해주세요!! ");
-    scanf("%s",s->phone_num);
-    printf("직책을 입력해주세요!! ");
-    scanf("%s",s->position);
-    printf("고유번호를 입력해주세요!! ");
-    scanf("%d",&s->special_num);
-    printf("비밀번호를 입력해주세요");
-    scanf("%s",s->pwd);
+int addPrivacy(Privacy *s)
+{
+    printf("이름을 입력해주세요 ");
+    scanf("%s", s->name);
+    printf("나이를 입력해주세요 ");
+    scanf("%d", &s->age);
+    printf("전화번호를 입력해주세요 ");
+    scanf("%s", s->phone_num);
+    printf("직책을 입력해주세요 ");
+    scanf("%s", s->position);
+    printf("고유번호를 입력해주세요 ");
+    scanf("%d", &s->special_num);
+    printf("비밀번호를 입력해주세요 ");
+    scanf("%s", s->pwd);
 
     return 1;
 }
-int delete_Privacy(Privacy *s){
-    s->age = -1;//나이가 음수인 거는 말이 되지 않음
-  return 1;
+int delete_Privacy(Privacy *s)
+{
+    s->age = -1; //나이가 음수인 거는 말이 되지 않음
+    return 1;
 }
 int updatePrivacy(Privacy *s)
 {
-    printf("새로운 이름을 입력해주세요!! ");
-    scanf("%s",s->name);
-    printf("새로운 나이를 입력해주세요!! ");
-    scanf("%d",&s->age);
-    printf("새로운 전화번호를 입력해주세요!! ");
-    scanf("%s",s->phone_num);
-    printf("새로운 직책을 입력해주세요!! ");
-    scanf("%s",s->position);
-    printf("새로운 비밀번호를 입력해주세요");
-    scanf("%s",s->pwd);
+    printf("새로운 이름을 입력해주세요 ");
+    scanf("%s", s->name);
+    printf("새로운 나이를 입력해주세요 ");
+    scanf("%d", &s->age);
+    printf("새로운 전화번호를 입력해주세요 ");
+    scanf("%s", s->phone_num);
+    printf("새로운 직책을 입력해주세요 ");
+    scanf("%s", s->position);
+    printf("새로운 비밀번호를 입력해주세요 ");
+    scanf("%s", s->pwd);
     printf("==> 수정되었습니다 <== \n");
 
     return 1;
